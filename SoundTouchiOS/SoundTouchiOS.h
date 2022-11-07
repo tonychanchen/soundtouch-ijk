1b3c79c7f510ab43e9ee55f64f56c6ea83d9f1b5
tonychan <tonychanchen@gmail.com>
2022-11-07 16:00:39 +0800

    动态库头文件

--- SoundTouchiOS/SoundTouchiOS.h
+++ SoundTouchiOS/SoundTouchiOS.h
@@ -14,5 +14,4 @@ FOUNDATION_EXPORT double SoundTouchiOSVersionNumber;
 FOUNDATION_EXPORT const unsigned char SoundTouchiOSVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <SoundTouchiOS/PublicHeader.h>
-
-
+#import <SoundTouchiOS/ijksoundtouch_wrap.h>
